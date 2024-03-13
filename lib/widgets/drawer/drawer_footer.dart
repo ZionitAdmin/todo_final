714e76aadee9544954c154d71048f37dd3548de5
sebastian Acuna <pinilla.sebastianm@gmail.com>
2024-03-12 21:28:49 -0300

    fix: footer safe area agregado

--- lib/widgets/drawer/drawer_footer.dart
+++ lib/widgets/drawer/drawer_footer.dart
@@ -8,30 +8,34 @@ class MyDrawerFooter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      child: CustomContainer(
-        color: Theme.of(context).colorScheme.surfaceVariant,
-        borderVariant: BorderVariant.all,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Switch(
-                value: context.read<ThemeProvider>().isInLightMode(),
-                onChanged: (_) => context.read<ThemeProvider>().toggleTheme(),
-                thumbIcon: MaterialStateProperty.resolveWith<Icon?>(
-                    (Set<MaterialState> states) {
-                  if (states.contains(MaterialState.selected)) {
-                    return const Icon(Icons.light_mode);
-                  }
-                  return const Icon(Icons
-                      .dark_mode); // All other states will use the default thumbIcon.
-                })),
-            const CircleAvatar(
-              child: Text("SA"),
-            )
-          ]),
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: CustomContainer(
+          color: Theme.of(context).colorScheme.surfaceVariant,
+          borderVariant: BorderVariant.all,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Switch(
+                      value: context.read<ThemeProvider>().isInLightMode(),
+                      onChanged: (_) =>
+                          context.read<ThemeProvider>().toggleTheme(),
+                      thumbIcon: MaterialStateProperty.resolveWith<Icon?>(
+                          (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.selected)) {
+                          return const Icon(Icons.light_mode);
+                        }
+                        return const Icon(Icons
+                            .dark_mode); // All other states will use the default thumbIcon.
+                      })),
+                  const CircleAvatar(
+                    child: Text("SA"),
+                  )
+                ]),
+          ),
         ),
       ),
     );
